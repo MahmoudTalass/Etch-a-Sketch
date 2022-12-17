bf6f91e54c6b835687cfe8a6e43a35bdee54107d
Mahmoud <mahmoud.talass@gmail.com>
2022-12-17 16:55:45 -0500

    Select all gridItems and loop over all of them
    Loop over all the items and add an event listner that
    listens to hovers over the items. When an item is hovered over the
    function 'highlight' gets activated and adds the class highlight to the
    gridItem so that it gets a black background.

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const container = document.querySelector("#container");
-
 createDivGrid();
+const gridItems = document.querySelectorAll(".grid-item");
+
+// gridItems.forEach(gridItem => {
+//    addEventListener("mouseover", (e) => {
+//     highlight(gridItem)
+//     console.log(e.target)
+// });
+// });
+
+for (let i = 0; i < gridItems.length; i++) {
+   gridItems[i].addEventListener("mouseover", () => highlight(gridItems[i]));
+}
 
 // function that creates a loop that runs 256 times at the beginning
 // of the program and creates 16x16 grid of divs
 function createDivGrid() {
-   for (let i = 0; i < 256; i++) {
-         let div = document.createElement("div");
-         div.classList.add("grid-item");
-         container.appendChild(div);
-        
+   for (let i = 0; i < 16 * 16; i++) {
+      let div = document.createElement("div");
+      div.classList.add("grid-item");
+      container.appendChild(div);
    }
+}
+
+// function that adds the highlight class to griditems so they
+// get background color black in the css
+function highlight(item) {
+   item.classList.add("highlight");
 }
